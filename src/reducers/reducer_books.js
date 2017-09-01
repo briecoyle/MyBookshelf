35aca45bed6eeddae9320757083717d257fc92c7
briecoyle <brie.coyle@gmail.com>
2017-08-31 20:13:21 -0400

    Add Books Reducer and connect using combineReducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "The Once and Future King" },
+    { title: "Downtown Owl" },
+    { title: "The Ghost Map" },
+    { title: "The Fellowship of the Ring" }
+  ]
+}
